140ea88f57837da54125f075725f5ae926d2939b
Michał Ławinski <michrarr@gmail.com>
2026-06-28 12:11:38 +0200

    Fix imports in data extractor module

--- src/modules/data-extractor.module.ts
+++ src/modules/data-extractor.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DataExtractorController } from 'src/controllers/data-extractor.controller';
+import { Settlement, SettlementSchema } from 'src/models/schemas/settlement.schema';
 import { FileExtractorService } from 'src/services/file-extractor.service';
 import { SettlementService } from 'src/services/settlement.service';
 
 @Module({
+      imports: [
+        MongooseModule.forFeature([
+          {
+            name: Settlement.name,
+            schema: SettlementSchema
+          }
+        ])
+      ],
     controllers: [DataExtractorController],
     providers: [SettlementService, FileExtractorService]
 })
